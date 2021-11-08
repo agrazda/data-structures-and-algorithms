a40a1e0ad83bf776b2dd39437094b25fd652e6c9
ALEX GRAZDA <AGRAZDA@GMAIL.COM>
2021-11-07 22:58:57 -0800

    binary search tree

--- javascript/401-code-challenges/trees/binaryTree.js
+++ javascript/401-code-challenges/trees/binaryTree.js
@@ -1,54 +1,52 @@
-'use strict';
+"use strict";
 
+const { insertAfter } = require("cheerio/lib/api/manipulation");
 
 class Node {
-    constructer(value){
-        this.value = value;
-        this.left = null;
-        this.right = null;
-    }
-
+  constructer(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
 }
 
 class BinaryTree {
-    constructor() {
-      this.root = null;
-    }
+  constructor() {
+    this.root = null;
   }
-  
-  function PreOrder(current) {
-    // console.log(current.value);
-    
-    if (current.left) {
-      PreOrder(current.left);
-    }
-    if (current.right) {
-      PreOrder(current.right);
-    }
+}
+
+function PreOrder(current) {
+  // console.log(current.value);
+
+  if (current.left) {
+    PreOrder(current.left);
   }
-  
-  function InOrder(current) {  
-    if (current.left) {
-      InOrder(current.left);
-    }
-  
-    // console.log(current.value);
-    
-    if (current.right) {
-      InOrder(current.right);
-    }
+  if (current.right) {
+    PreOrder(current.right);
   }
-  
-  function PostOrder(current) {  
-    if (current.left) {
-      PostOrder(current.left);
-    }
-    
-    if (current.right) {
-      PostOrder(current.right);
-    }
-    // console.log(current.value);
+}
+
+function InOrder(current) {
+  if (current.left) {
+    InOrder(current.left);
   }
+  // console.log(current.value);
+
+  if (current.right) {
+    InOrder(current.right);
+  }
+}
+
+function PostOrder(current) {
+  if (current.left) {
+    PostOrder(current.left);
+  }
+  if (current.right) {
+    PostOrder(current.right);
+  }
+  // console.log(current.value);
+}
 
 //   let tree = new BinaryTree();
 
@@ -72,9 +70,17 @@ class BinaryTree {
 // PostOrder(tree.root);
 
 class BinarySearchTree {
-    constructor() {
-        this.root = null;
-    }
+  constructor() {
+    this.root = null;
+  }
 }
 
-module.exports(Node, BinaryTree, BinarySearchTree)
+    insert(value){
+        let newNode = new Node(value);
+        if(this.root === null){
+            this.root = newNode;
+            return this;
+        }
+    }
+
+module.exports(BinaryTree, BinarySearchTree);
