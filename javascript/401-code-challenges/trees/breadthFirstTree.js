a9998448ba81de6b456568cd1c714c5768d1ec71
ALEX GRAZDA <AGRAZDA@GMAIL.COM>
2021-11-10 15:32:52 -0800

    Fizz Buzz

--- javascript/401-code-challenges/trees/breadthFirstTree.js
+++ javascript/401-code-challenges/trees/breadthFirstTree.js
@@ -1,9 +1,10 @@
 'use strict';
 
+
 class Node {
     constructor(value, k = 0) {
         this.value = value;
-        this.children = new Array(k); [undefined, undefined];
+        this.children = new Array(k);
     }
 }
 
@@ -17,23 +18,44 @@ class KaryTree {
   function breadthFirst(root) {
     // have a place to store current children
     const queue = [];
+    let array = [];
+
   
     let current = root;
     queue.unshift(root);
     while(queue.length) {
       // take a node off the queue
       current = queue.pop();
+      array.push(current.value);
   
       // read it's value
-      console.log(current.value);
+      // console.log(current.value);
   
       // push all children into queue.
       for (let node of current.children) {
         queue.unshift(node);
       }
     }
+      return array;
   }
-  
+
+
+    function fizzBuzz(kary) {
+      let result = [];
+      let i;
+      console.log('***************', kary);
+      for(i=0; i < kary.length; i++) {
+        if((kary[i]%15)==0)
+          result.push('FizzBuzz');
+         else if ((kary[i]%3)==0)
+          result.push('Fizz');
+         else if ((kary[i]%5)==0)
+          result.push('Buzz');
+         else
+          result.push(kary[i].toString());   
+      }
+      return result
+    }
   let kary = new KaryTree(2);
   
   kary.root = new Node(1);
@@ -44,9 +66,12 @@ class KaryTree {
   kary.root.children[1].children[0].children.push(new Node(8), new Node(9));
   
   
-  breadthFirst(kary.root);
+  console.log(breadthFirst(kary.root));
+  //Breadth Fisrt Output: 1, 2, 6, 3, 5, 7, 4, 8, 9
+  console.log(fizzBuzz(breadthFirst(kary.root)));
+  // fizzBuss Output: 1, 2, Fizz, Fizz, Buzz, 7, 4, 8, Fizz 
 
-// Output: 1, 2, 6, 3, 5, 7, 4, 8, 9  
+  
 
 //         1
 //        / \
@@ -55,3 +80,4 @@ class KaryTree {
 //     3   5   7
 //    /       / \
 //   4       8   9
+
