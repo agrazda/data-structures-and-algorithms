9357d16415f29cb83ec605d68c189b5f422c9476
ALEX GRAZDA <AGRAZDA@GMAIL.COM>
2021-12-05 21:26:52 -0800

    hash map

--- javascript/401-code-challenges/combineHasmaps/combineHashmaps.js
+++ javascript/401-code-challenges/combineHasmaps/combineHashmaps.js
@@ -1,2 +1,16 @@
-'use strict';
+"use strict";
 
+const HashTable = require('..hashTable/hashTable.js');
+
+function leftJoin(hashMap1, hashMap2) {
+  let arr = [];
+
+  hashMap1.map.forEach((value) => {
+    Object.getOwnPropertyNames(value.head.value).forEach((prop) => {
+      arr.push([prop, hashMap1.get(prop), hashMap2.get(prop)]);
+    });
+  });
+
+  return arr;
+}
+module.exports = leftJoin;
